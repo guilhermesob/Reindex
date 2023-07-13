a5036c9a0dd9dfb4372c389b4e048f4ccdc5e66a
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-13 20:56:12 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+
+- index.php 
+- .htaccess 
++ conf
+  |- application.ini //application config
+- application/
+  - Bootstrap.php   
+  + controllers
+     - Index.php //default controller
+  + views    
+     |+ index   
+        - index.phtml //view template for default action
+  + modules 
+  - library
+  - models  
+  - plugins 
