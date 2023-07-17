1e6ea0240aa421cfc98a944233842ee36689bb23
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-17 20:43:00 -0300

    Create index_json.php

---
+++ index_json.php
@@ -0,0 +1,14 @@
+index3.php
+$json='{
+"name": "John Doe".
+"age":30,
+"city":"San Francisco"
+}';
+
+$key = "name";
+
+if (isset(#json[$key])) {
+ echo "The key $key exixt.";
+} else {
+ echo "The key $key does not exist.";
+{
